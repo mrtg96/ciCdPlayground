07af6e0f12b5714dfdc713afd97e6b3c666a7382
mrtg96 <marvin.reuner@tngtech.com>
2025-08-22 10:39:04 +0200

    Fill in the personal Data

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Walter Herfordt",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
     associations: ["Ehm, what?"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Jenkins",
     // What do you want to learn in this workshop?
     expectations: ["what you are talking about"],
   },
